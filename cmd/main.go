04d4cd88a2ca62c6703bc6789885fe9aee2b24b7
Evan Lezar <evan.lezar@zalando.de>
2016-11-29 16:03:57 +0100

    Use ddo/fast-com for testing internet speed

--- cmd/main.go
+++ cmd/main.go
@@ -1,87 +1,75 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"io"
 	"log"
-	"os"
-	"os/exec"
-	"regexp"
-	"strconv"
-	"strings"
+
 	"time"
+
+	fast "gopkg.in/ddo/go-fast.v0"
 )
 
-// runs the given command and writes the stdout to the given outputPath
-func runner(cmdString, outputPath string) error {
-	cmdFields := strings.Fields(cmdString)
-	cmdName := cmdFields[0]
-	cmdParams := cmdFields[1:]
-
-	f, err := os.OpenFile(outputPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Println("Error opening file: ", outputPath)
-		fmt.Println(err)
-		return err
+func process(speeds <-chan float64, errors <-chan error) {
+	for {
+		select {
+		case mbps := <-speeds:
+			log.Printf("SPEED (Mbps): %.2f\n", mbps)
+		case err := <-errors:
+			log.Println("ERROR: ", err)
+		}
 	}
-	defer f.Close()
+}
 
-	log.SetOutput(f)
+func runTest(speeds chan<- float64, errors chan<- error) {
+	fastCom := fast.New()
 
-	log.Println("### Starting:")
-	defer log.Println("### Done:")
-
-	cmd := exec.Command(cmdName, cmdParams...)
-	output := io.MultiWriter(f, os.Stdout)
-	cmd.Stdout = output
-	cmd.Stderr = output
-	err = cmd.Run()
+	err := fastCom.Init()
 	if err != nil {
-		fmt.Fprintln(output, "Error running: ", cmdString)
-		fmt.Fprintln(output, err, cmdString)
+		errors <- err
 	}
 
-	return nil
+	// get urls
+	urls, err := fastCom.GetUrls()
+	if err != nil {
+		errors <- err
+	}
+
+	// measure
+	KbpsChan := make(chan float64)
+	go func() {
+		for Kbps := range KbpsChan {
+			speeds <- Kbps / 1024
+		}
+	}()
+
+	err = fastCom.Measure(urls, KbpsChan)
+	if err != nil {
+		errors <- err
+	}
 
 }
 
-// executes the given command periodically and writes the stdout to the specified file
 func main() {
-	command := flag.String("c", "speedtest-cli", "Command that will be exectued.")
-	period := flag.String("p", "10m", "Period in which the command will be executed in.")
-	outputPath := flag.String("o", "go-faster.log", "File in which the data should go.")
+	errors := make(chan error)
+	speeds := make(chan float64)
+	force := make(chan bool)
 
-	flag.Parse()
+	go func() {
+		force <- true
+		process(speeds, errors)
+	}()
 
-	properPeriodRegex, _ := regexp.Compile("\\d+[s,m,h,d]")
-
-	if !properPeriodRegex.MatchString(*period) {
-		panic("period has to match '\\d+[s,m,h,d]'")
+	for {
+		run := false
+		select {
+		case <-force:
+			run = true
+		case <-time.After(10 * time.Second):
+			run = true
+		}
+		if run {
+			runTest(speeds, errors)
+			log.Println("After runTest")
+		}
 	}
 
-	periodUnitRegex, _ := regexp.Compile("\\d+")
-	periodValueRegex, _ := regexp.Compile("[smhd]")
-
-	periodValue, _ := strconv.ParseInt(periodValueRegex.ReplaceAllString(*period, ""), 10, 64)
-	periodUnit := string(periodUnitRegex.ReplaceAllString(*period, ""))
-
-	var periodDuration int64
-
-	switch periodUnit {
-	case "s":
-		periodDuration = periodValue * int64(time.Second)
-	case "m":
-		periodDuration = periodValue * int64(time.Minute)
-	case "h":
-		periodDuration = periodValue * int64(time.Hour)
-	case "d":
-		periodDuration = periodValue * int64(time.Hour) * 24
-	}
-
-	runner(*command, *outputPath)
-
-	for _ = range time.Tick(time.Duration(periodDuration)) {
-		runner(*command, *outputPath)
-	}
 }
